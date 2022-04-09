04e131621c0f35788cf3435289bedf25fdb9bcb3
austinjs <ajsprunger@gmail.com>
2022-04-09 12:54:49 -0600

    add user model, login/out, initial db setup

---
+++ myapp/models.py
@@ -0,0 +1,41 @@
+#models 
+from myapp import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+#allows to set up isAuthenticate etc 
+from flask_login import UserMixin
+from datetime import datetime
+
+#login management 
+# allows us to use this in templates for isUser stuff 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+#going to use this in our login view 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f"Username {self.username}"
+
+class Cookout(db.Model):
+    __tablename__ = 'cookouts'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now() + datetime.timedelta(days=7))
+    name = db.Column(db.String(100), nullable=False)
+    creator = db.Column(db.Integer, db.ForeignKey('users.Id'), nullable=False)
+    location = db.Column(db.String(150))
+    food = db.Column(db.List)
