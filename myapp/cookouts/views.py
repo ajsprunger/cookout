a7a1fd8c6ab21fc2603af7a9d6ee5e501652a037
austinjs <ajsprunger@gmail.com>
2022-04-11 10:40:54 -0600

    add update and delete cookout functions

--- myapp/cookouts/views.py
+++ myapp/cookouts/views.py
@@ -18,3 +18,47 @@ def create_cookout():
         print('Cookout was created')
         return redirect(url_for('core.index'))
     return render_template('create_cookout.html', form=form)
+
+@cookouts.route('/<int:cookout_id>')
+def cookout(cookout_id):
+    cookout = Cookout.query.get_or_404(cookout_id) 
+    return render_template('cookout.html', name=cookout.name, date=cookout.date, description=cookout.description, creator=cookout.creator, location=cookout.location, food=cookout.food, drink=cookout.drink, attendees=cookout.attendees)
+
+@cookouts.route('/<int:cookout_id>/update',methods=['GET','POST'])
+@login_required
+def update(cookout_id):
+    cookout = Cookout.query.get_or_404(cookout_id)
+    if cookout.author != current_user:
+        abort(403)
+    form = CookoutForm()
+    if form.validate_on_submit():
+        cookout.name = form.name.data
+        cookout.description = form.description.data
+        cookout.date = form.date.data
+        cookout.location = form.location.data
+        cookout.food = form.food.data
+        cookout.drink = form.drink.data
+        cookout.attendees = form.attendees.data
+        db.session.commit()
+        flash('Cookout Updated')
+        return redirect(url_for('cookouts.cookout',cookout_id=cookout.id))
+    elif request.method == 'GET':
+        form.name.data = cookout.name
+        form.description.data = cookout.description
+        form.date.data = cookout.date
+        form.location.data = cookout.location
+        form.food.data = cookout.food
+        form.drink.data = cookout.drink
+        form.attendees = cookout.attendees
+    return render_template('create_cookout.html',title='Updating',form=form)
+
+@cookouts.route('/<int:cookout_id>/delete',methods=['GET','POST'])
+@login_required
+def delete_post(cookout_id):
+    cookout = Cookout.query.get_or_404(cookout_id)
+    if cookout.creator != current_user:
+        abort(403)
+    db.session.delete(cookout)
+    db.session.commit()
+    flash('Cookout Deleted')
+    return redirect(url_for('core.index'))
--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,14 +1,16 @@
 # core/views.py 
 
 from flask import render_template, request, Blueprint
+from myapp.models import Cookout
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
-
+    page = request.args.get('page', 1, type=int)
+    cookouts = Cookout.query.order_by(Cookout.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', cookouts=cookouts)
+        
 @core.route('/info')
 def info():
     return render_template('info.html')
